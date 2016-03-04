2db1b4aea8da54d5c1ad8e8c56a64413adf34c86
Drew Beckmen <dbeckmen>
2016-03-03 20:33:37 -0500

    Added enhancement of asking whether to divide using remainders or decimals.

--- fancyDivision.py
+++ fancyDivision.py
@@ -1,17 +1,28 @@
-#Author: Drew Beckmen
-#Date: 2/4/16
+remainderOrDemicals = raw_input("Would you like to divide calculating remainders or have the answer include decimals? Type remainders for the first and decimals for the second option: ")
+remainderOrDemicals.lower()
 
-dividend = int(raw_input("Enter a dividend: "))
-divisor = int(raw_input("Enter a divisor: "))
+dividend = raw_input("Enter a dividend: ")
+divisor = raw_input("Enter a divisor: ")
+#try:
+	#dividend = int(dividend)
+#except ValueError: 
+	#dividend = raw_input("Ynot a real number. Try again: ")
 
-#Since dividend and divisor are int, the variable wholeNumberAnswer will be the rounded down number
-wholeNumberAnswer = dividend/divisor
+if remainderOrDemicals == "remainders":
+	#Since dividend and divisor are int, the variable wholeNumberAnswer will be the rounded down number
+	wholeNumberAnswer = dividend/divisor
 
-#Using the mod operator to calculate the remainder. No other calculations are needed. 
-remainder = dividend%divisor
+	#Using the mod operator to calculate the remainder. No other calculations are needed. 
+	remainder = dividend%divisor
 
-#Using format strings avoids the need to change all variables back into strings
-if remainder == 0:
-	print("{} divided by {} is {}." .format(dividend, divisor, wholeNumberAnswer))
-else:
-	print("{} divided by {} is {} with a remainder of {}." .format(dividend, divisor, wholeNumberAnswer, remainder))
+	#Using format strings avoids the need to change all variables back into strings
+	if remainder == 0:
+		print("{} divided by {} is {}." .format(dividend, divisor, wholeNumberAnswer))
+	else:
+		print("{} divided by {} is {} with a remainder of {}." .format(dividend, divisor, wholeNumberAnswer, remainder))
+if remainderOrDemicals == "decimals": 
+	dividend = float(dividend)
+	divisor = float(divisor)
+	decimalAnswer = dividend/divisor
+	decimalAnswer = round(decimalAnswer,2)
+	print("{} divided by {} is {}." .format(dividend, divisor, decimalAnswer))
