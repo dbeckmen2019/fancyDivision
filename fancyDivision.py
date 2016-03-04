6c577e30b953c23e53eefab0fa1e4a07a19b2a5e
Drew Beckmen <dbeckmen>
2016-03-03 20:49:54 -0500

    Modified comments to be more concise and fit code better.

--- fancyDivision.py
+++ fancyDivision.py
@@ -1,5 +1,5 @@
 remainderOrDemicals = raw_input("Would you like to divide calculating remainders or have the answer include decimals? Type remainders for the first and decimals for the second option: ")
-remainderOrDemicals.lower() #so any capital letters typed by user will be converted to lowercase
+remainderOrDemicals.lower() #any capital letters typed by user will be converted to lowercase
 
 x = 4 #for the conditions on our loops
 y = 4
@@ -8,7 +8,7 @@ dividend = raw_input("Enter a dividend: ")
 while x != 1: #will tell user when the number they entered is not a real number rather than ending program with error
 	try:
 		dividend = int(dividend)
-		x=1
+		x=1 #will break out of loop
 	except ValueError: 
 		dividend = raw_input("The dividend you entered is not a real number. Try again: ")
 
@@ -20,19 +20,18 @@ while y != 1: #same block as above, but for the divisor rather than the dividend
 	except ValueError: 
 		divisor = raw_input("The divisor you entered is not a real number. Try again: ")
 
-#All code for dividing while calculating remainders. 
+#Dividing while calculating remainders. 
 if remainderOrDemicals == "remainders":
 	#Since dividend and divisor are int, the variable wholeNumberAnswer will be the rounded down number
 	wholeNumberAnswer = dividend / divisor
-	#Using the mod operator to calculate the remainder. No other calculations are needed. 
+	#Mod operator to calculate the remainder
 	remainder = dividend % divisor
-	#Using format strings avoids the need to change all variables back into strings
-	if remainder == 0:
+	if remainder == 0: #there is no need to print that the remainder is 0. 
 		print("{} divided by {} is {}." .format(dividend, divisor, wholeNumberAnswer))
 	else:
 		print("{} divided by {} is {} with a remainder of {}." .format(dividend, divisor, wholeNumberAnswer, remainder))
 
-#The code to divide using decimals. 
+#Divide using decimals. 
 if remainderOrDemicals == "decimals": 
 	dividend = float(dividend) #have to convert to floats in order to get decimals
 	divisor = float(divisor)
